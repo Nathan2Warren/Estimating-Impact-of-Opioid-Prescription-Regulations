7129b826a0810d8334e1db31cd3c581db020556d
Nathan2Warren <Nathan.warren222@gmail.com>
2019-10-31 00:05:51 -0400

    Finished data cleaning and very close to done with the merge

--- 10_code/25_Data_Cleaning.py
+++ 10_code/25_Data_Cleaning.py
@@ -3,7 +3,7 @@ import os
 
 #Change to directory for ease of loading files
 os.chdir('/Users/N1/Op690/estimating-impact-of-opioid-prescription-regulations-team-3/00_source/')
-
+pd.set_option('display.max_columns', None)
 ########################################################################################################################
 # FL_Opiate cleaning
 ########################################################################################################################
@@ -11,33 +11,41 @@ os.chdir('/Users/N1/Op690/estimating-impact-of-opioid-prescription-regulations-t
 # FL_Opiate load
 FL_Opiate = pd.read_parquet("FLopiate.parquet", engine = 'fastparquet')
 
-# Dropped because they are not useful for the questions we are trying to answer
-FL_Opiate_Droplist = ['REPORTER_ADDL_CO_INFO', 'UNIT', 'TRANSACTION_CODE',
-                      'Unnamed: 0', 'REPORTER_ADDL_CO_INFO', 'REPORTER_DEA_NO',
-                      'REPORTER_ADDRESS1', 'REPORTER_ADDRESS2', 'DRUG_CODE', 'Measure']
+# Drop Duplicates - there are no duplicates
+#FL_Opiate.shape
+#FL_Opiate = FL_Opiate.drop_duplicates()
+#FL_Opiate.shape
 
-FL_Opiate = FL_Opiate.drop(FL_Opiate_Droplist, axis = 1)
+# Only Columns we needed, When we extract the data for other states we should only open these columns (maybe less)
+FL_Opiate = FL_Opiate[['BUYER_COUNTY','DRUG_NAME', 'QUANTITY', 'TRANSACTION_DATE',
+                       'CALC_BASE_WT_IN_GM', 'DOSAGE_UNIT', 'Ingredient_Name',
+                       'MME_Conversion_Factor','dos_str', 'Revised_Company_Name'
+                       ,'BUYER_STATE']]
 
-# Drop Duplicates
-FL_Opiate = FL_Opiate.drop_duplicates()
-FL_Opiate.columns
-
-#Drop missing County values
-FL_Opiate[FL_Opiate['County'] == None]
-FL_Opiate = FL_Opiate[FL_Opiate['County'] != None]
-
-# Convert to string so can use str.extract to separate by year
-FL_Opiate['TRANSACTION_DATE'] = FL_Opiate['TRANSACTION_DATE'].astype('str')
-
-# Extract year from the string
-FL_Opiate['Year'] = FL_Opiate['TRANSACTION_DATE'].str.extract('([0-9]{4}$)', expand=True)
-FL_Opiate.head(5)
-
-# Extract State from TRANSACTION_DATE -- no longer needed when we changed the file
+# Extract State from BUYER_COUNTY -- no longer needed when we changed the file
 #FL_Mort['State'] = FL_Mort['County'].str.extract('([A-Z]{2}$)', expand=True)
 #FL_Mort.head(5)
 
-#Save to Parquet file
+# Extract year from the TRANSACTION_DATE string
+FL_Opiate['TRANSACTION_DATE'] = FL_Opiate['TRANSACTION_DATE'].astype('str')
+FL_Opiate['Year'] = FL_Opiate['TRANSACTION_DATE'].str.extract('([0-9]{4}$)', expand=True)
+FL_Opiate.head(5)
+
+# Rename so we can merge later
+FL_Opiate.rename(columns = {'BUYER_STATE':'State'}, inplace = True)
+FL_Opiate.rename(columns = {'BUYER_COUNTY':'County'}, inplace = True)
+FL_Opiate.head()
+
+# Get MME and drop columns that are no longer needed
+FL_Opiate['MME'] = FL_Opiate.DOSAGE_UNIT * FL_Opiate.MME_Conversion_Factor
+FL_Opiate.drop(['DOSAGE_UNIT', 'MME_Conversion_Factor'], axis=1, inplace=True)
+FL_Opiate.head()
+
+# Drop missing County values
+FL_Opiate[FL_Opiate['County'] == None]
+FL_Opiate = FL_Opiate[FL_Opiate['County'] != None]
+
+# Save to Parquet file
 FL_Opiate.to_parquet("FLopiate cleaned.parquet", engine = 'fastparquet')
 
 
@@ -45,6 +53,29 @@ FL_Opiate.to_parquet("FLopiate cleaned.parquet", engine = 'fastparquet')
 ### FL_Mort cleaning
 ########################################################################################################################
 
-FL_Mort_load = pd.read_csv("FL_mortality_2003_2015.csv")
-FL_Mort_Droplist = ['Unnamed:0', 'Notes', 'County Code', 'Year Code']
-FL_Mort = FL_Mort.drop_duplicates()
+#Load and Drop useless columns
+FL_Mort = pd.read_csv("FL_mortality_2003_2015.csv")
+FL_Mort_Droplist = ['Unnamed: 0', 'Notes', 'County Code', 'Year Code']
+FL_Mort.head(5)
+FL_Mort = FL_Mort.drop(FL_Mort_Droplist, axis = 1)
+
+# Change from lower case to upper case so that the merged values in 'County' will match
+FL_Mort['County'] = FL_Mort['County'].str.upper()
+FL_Mort['County'] = FL_Mort['County'].str.rstrip(' COUNTY')
+
+# Make a replace dictionary fix the different naming
+Replace_list = ['WAL', 'LE', 'ST.JOHNS', 'ST. LUCIE', 'BA', 'CLA', 'LEV', 'MARI', 'MARTI', 'NASSA', 'PAS']
+Replace_fixed = ['WALTON', 'LEE', 'SAINT JOHNS', 'SAINT LUCIE', 'BAY', 'CLAY', 'LEVY', 'MARION', 'MARTIN', 'NASSAU', 'PASCO']
+Replace_dict = dict(zip(Replace_list, Replace_fixed))
+
+#Replace Counties with proper names
+FL_Mort['County'].replace(Replace_dict, inplace = True)
+
+#Drop Years beyond 2012 from Mort
+FL_Mort['Year'] = FL_Mort['Year'].astype(int)
+FL_Mort = FL_Mort[FL_Mort['Year'] < 2013]
+FL_Mort['Year'] = FL_Mort['Year'].astype(str)
+
+FL_Mort.shape
+
+FL_Mort.to_csv("20_FL_Mort cleaned.csv")
--- 10_code/30_Merge.py
+++ 10_code/30_Merge.py
@@ -7,8 +7,8 @@ os.chdir('/Users/N1/Op690/estimating-impact-of-opioid-prescription-regulations-t
 pd.set_option('display.max_columns', None)
 
 # Read Files
-FL_Mort_load = pd.read_csv("FL_mortality_2003_2015.csv")
-FL_Opiate_load = pd.read_parquet("FLopiate.parquet", engine = 'fastparquet')
+FL_Mort_load = pd.read_csv("20_FL_Mort cleaned.csv")
+FL_Opiate_load = pd.read_parquet("FLopiate cleaned.parquet", engine = 'fastparquet')
 
 # Do this so don't have to make reload from parquet when working
 FL_Mort = FL_Mort_load.copy()
@@ -16,80 +16,55 @@ FL_Opiate = FL_Opiate_load.copy()
 FL_Mort.head()
 FL_Opiate.head()
 
-##### FL_Opiate Editing
-
+########################################################################################################################
+# GROUPBY
+########################################################################################################################
 # Rename so that we can merge easily on these columns
-FL_Opiate.rename(columns = {'BUYER_STATE':'State'}, inplace = True)
-FL_Opiate.rename(columns = {'BUYER_COUNTY':'County'}, inplace = True)
-
-######### FL_Mort Editing
-# Change types to strings so we can edit
-FL_Mort['County'] = FL_Mort['County'].astype(str)
-FL_Mort['Year'] = FL_Mort['Year'].astype(str)
-FL_Mort['State'] = FL_Mort['State'].astype(str)
-FL_Opiate.head()
 FL_Opiate['State'] = FL_Opiate['State'].astype(str)
 FL_Opiate['Year'] = FL_Opiate['Year'].astype(str)
 FL_Opiate['County'] = FL_Opiate['County'].astype(str)
 
-# Change from lower case to upper case so that the merged values in 'County' will match
-FL_Mort['County'] = FL_Mort['County'].str.upper()
-FL_Mort['County'] = FL_Mort['County'].str.rstrip(' COUNTY')
+FL_Opiate_Grouped = FL_Opiate.groupby(['County', 'Year', 'State']).sum().reset_index()
+FL_Opiate_Grouped.shape
+FL_Opiate_Grouped.head(20)
 
-# Drop Duplicates
-FL_Mort = FL_Mort.drop_duplicates()
+# Change types to strings so we can edit
+FL_Mort['County'] = FL_Mort['County'].astype(str)
+FL_Mort['Year'] = FL_Mort['Year'].astype(str)
+FL_Mort['State'] = FL_Mort['State'].astype(str)
 
-FL_Mort.shape
-FL_Opiate.shape
-
-##### Creating Test for merging
-FL_Mort_Test = FL_Mort[['State', 'Year', 'County']]
-FL_Opiate_Test = FL_Opiate[['State', 'Year', 'County']]
-
-#Opiate_Test only goes till 2012 while Mort goes till 2015
-FL_Mort_Test['Year'].unique()
-FL_Opiate_Test['Year'].unique()
-
-#Drop Mort beyond 2012 from Mort
-FL_Mort_Test['Year'] = FL_Mort_Test['Year'].astype(int)
-FL_Mort_Test = FL_Mort_Test[FL_Mort_Test['Year'] < 2013]
-FL_Mort_Test['Year'] = FL_Mort_Test['Year'].astype(str)
+########################################################################################################################
+# Merging
+########################################################################################################################
 
 #These match so that isnt the problem
-FL_Mort_Test['State'].unique()
-FL_Opiate_Test['State'].unique()
+FL_Mort['State'].unique()
+FL_Opiate['State'].unique()
 
 # Big Difference in the amount of counties in each and the spelling & abbreviations used
-sorted(FL_Mort_Test['County'].unique())
-sorted(FL_Opiate_Test['County'].unique())
+sorted(FL_Mort['County'].unique())
+sorted(FL_Opiate_Grouped['County'].unique())
 
-# Make a replace dictionary fix the different naming
-Replace_list = ['WAL', 'LE', 'ST.JOHNS', 'ST. LUCIE', 'BA', 'CLA', 'LEV', 'MARI', 'MARTI', 'NASSA', 'PAS']
-Replace_fixed = ['WALTON', 'LEE', 'SAINT JOHNS', 'SAINT LUCIE', 'BAY', 'CLAY', 'LEVY', 'MARION', 'MARTIN', 'NASSAU', 'PASCO']
-Replace_dict = dict(zip(Replace_list, Replace_fixed))
+# Dropping Counties that are not in opiate for mort
+FL_Opiate_County_Unique = FL_Opiate_Grouped['County'].unique()
+FL_Opiate_County_Unique
 
-#Replace Counties
-FL_Mort_Test['County'].replace(Replace_dict, inplace = True)
+FL_Mort_County_Unique = FL_Mort['County'].unique()
+FL_Mort_County_Unique
 
-sorted(FL_Mort_Test['County'].unique())
-sorted(FL_Opiate_Test['County'].unique())
+#Look at similarities and differences to see if we missed anything
+Intersection = set(FL_Mort_County_Unique).intersection(FL_Opiate_County_Unique)
+Differences = set(FL_Mort_County_Unique).symmetric_difference(FL_Opiate_County_Unique)
 
-# Dropping Counties that do not match // NOT DONE YET
-keepers = ['ALACHUA', 'BAY', 'BREVARD', 'BROWARD', 'CITRUS', 'CLAY', 'COLLIER',
-       'DUVAL', 'ESCAMBIA', 'HERNAND', 'HIGHLANDS', 'HILLSBOROUGH',
-       'INDIAN RIVER', 'LAKE', 'LEE', 'MANATEE', 'MARI', 'MARTI',
-       'MIAMI-DADE', 'MONROE', 'OKALOOSA', 'ORANGE', 'OSCEOLA',
-       'PALM BEACH', 'PAS', 'PINELLAS', 'POLK', 'ST. LUCIE', 'SANTA ROSA',
-       'SARASOTA', 'SEMINOLE', 'VOLUSIA', 'CHARLOTTE', 'NASSAU', 'PUTNAM',
-       'SAINT JOHNS', 'COLUMBIA', 'WALTON', 'OKEECHOBEE', 'FLAGLER', 'LEV']
+FL_Mort = FL_Mort[FL_Mort['County'].isin(Intersection)]
+FL_Opiate_Grouped = FL_Opiate_Grouped[FL_Opiate_Grouped['County'].isin(Intersection)]
 
+# Merge
+FL_Merge = pd.merge(FL_Mort, FL_Opiate_Grouped, how = 'outer', on = ['County', 'Year', 'State'], indicator = True)
 
-FL_Opiate_Test = FL_Opiate_Test[FL_Opiate_Test['County'].isin(keepers)]
-FL_Mort_Test = FL_Mort_Test[FL_Mort_Test['County'].isin(keepers)]
-
-
-# Merge (we should take out state since it is already all Florida)
-FL_Merge = pd.merge(FL_Mort_Test, FL_Opiate_Test, how = 'outer', on = ['County', 'Year', 'State'], indicator = True)
+########################################################################################################################
+# Troubleshooting
+########################################################################################################################
 
 # Check if it worked
 FL_Merge['_merge'].unique
@@ -101,8 +76,28 @@ FL_Merge[FL_Merge['_merge'] == 'both']
 FL_Merge_Left = FL_Merge[FL_Merge['_merge'] == 'left_only']
 FL_Merge_Left
 
+# Missing years for all below, have not checked all yet
+FL_Opiate_Grouped[FL_Opiate_Grouped['County'] == 'ALACHUA']
+FL_Mort[FL_Mort['County'] == 'ALACHUA']
+
+FL_Opiate_Grouped[FL_Opiate_Grouped['County'] == 'BAY']
+FL_Mort[FL_Mort['County'] == 'BAY']
+
+FL_Opiate_Grouped[FL_Opiate_Grouped['County'] == 'CHARLOTTE']
+FL_Mort[FL_Mort['County'] == 'CHARLOTTE']
+
+FL_Opiate_Grouped[FL_Opiate_Grouped['County'] == 'COLUMBIA']
+FL_Mort[FL_Mort['County'] == 'COLUMBIA']
+
+FL_Opiate_Grouped[FL_Opiate_Grouped['County'] == 'FLAGLER']
+FL_Mort[FL_Mort['County'] == 'FLAGLER']
+
+FL_Opiate_Grouped[FL_Opiate_Grouped['County'] == 'HIGHLANDS']
+FL_Mort[FL_Mort['County'] == 'HIGHLANDS']
+
 # FL_Opiate
 FL_Merge_Right = FL_Merge[FL_Merge['_merge'] == 'right_only']
+FL_Merge_Right.shape
 FL_Merge_Right
 
 FL_Merge_Left['County'].unique()
