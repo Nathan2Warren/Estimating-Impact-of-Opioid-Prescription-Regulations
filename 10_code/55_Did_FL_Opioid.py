efd9b297310466005f566ff60f1f8ffd5034e485
jz263 <54601170+jz263@users.noreply.github.com>
2019-11-21 21:30:56 -0500

    Added and modified all files and codes for did analysis

--- 10_code/55_Did_FL_Opioid.py
+++ 10_code/55_Did_FL_Opioid.py
@@ -7,7 +7,7 @@ pd.set_option('display.max_columns', None)
 #define states, the first is study state, the latters are comparison states
 States = ['FL','GA','IL','IN','OH','PA']
 policy_year = 2010
-os.chdir('C:/Users/Jiajie Zhang/IDS_mid_project/Intermediate_data')
+os.chdir('C:/Users/Jiajie Zhang/estimating-impact-of-opioid-prescription-regulations-team-3/20_intermediate_files')
 os.getcwd()
 
 #read in opioid data
@@ -17,13 +17,13 @@ for filename in glob.glob('./*cleaned_and_merged.csv'):
     df_tmp = pd.read_csv(filename)
     Opioid_Mort = Opioid_Mort.append(df_tmp)
 #checkout if all states are included
-Opioid_Mort['State'].value_counts()
+#Opioid_Mort['State'].value_counts()
 Opioid_Mort = Opioid_Mort.drop(columns = 'Unnamed: 0')
 
 ## There are two rows with county = NaN, dorp it.
 Opioid_Mort = Opioid_Mort.drop(Opioid_Mort.loc[Opioid_Mort['County'].isnull()].index)
 #read in Census data
-Census_full = pd.read_csv('./Census_Full_State_Level.csv')
+Census_full = pd.read_csv('./21_Census_Full_County_Level.csv')
 
 # subset needed states
 mask = Census_full['State'].isin(States)
@@ -36,9 +36,8 @@ Census_sel = Census_sel.drop(columns = 'Unnamed: 0')
 
 # Merge Opioid_Mort with Census
 Opioid_Mort_Census = pd.merge(Opioid_Mort,Census_sel,on =['Year','State','County'],how = 'left')
-##test = pd.merge(Opioid_Mort,Census_sel,on =['Year','State','County'],how = 'outer')
 ##test['County'].unique()
-
+##test = pd.merge(Opioid_Mort,Census_sel,on =['Year','State','County'],how = 'outer')
 
 # We'vedone combining all data needed for FL Opioid & Deaths Did, next cal value per-cap for deaths and MME
 
@@ -59,25 +58,25 @@ Opioid_Mort_Census['Year'].isnull().sum()
 
 #stack two dataframes
 #Opioid_Mort_Census = pd.concat([Study_Opioid_Mort_Census,Compare_grouped],sort = False)
-line_a = Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)]
-len(sorted(line_a['MME_Per_Capita'],reverse = True))
-line_a['MME_Per_Capita'].isnull().sum()
+#line_a = Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)]
+#len(sorted(line_a['MME_Per_Capita'],reverse = True))
+#line_a['MME_Per_Capita'].isnull().sum()
 p = (ggplot(Opioid_Mort_Census,aes( x = 'Year', y ='MME_Per_Capita')) +
 #geom_line(Opioid_Mort_Census.loc[Opioid_Mort_Census['after_policy_year']==True], aes( x = 'Year', y ='Pred_Deaths_per_Pop'), color = "red") +
-geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y = 'MME_Per_Capita'), method = 'lm', color = "black") +
-geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y ='MME_Per_Capita'), method = 'lm',color = "black") +
-geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y = 'MME_Per_Capita'), method = 'lm',color = "blue") +
-geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y ='MME_Per_Capita'), method = 'lm',color = "blue") +
-geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y = 'MME_Per_Capita'), color = "black") +
-geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y ='MME_Per_Capita'),color = "black") +
-geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y = 'MME_Per_Capita'), color = "blue") +
-geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y ='MME_Per_Capita'),color = "blue") +
+geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y = 'MME_Per_Capita', color = 'Is_Target_State'), method = 'lm') +
+geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y ='MME_Per_Capita',color = 'Is_Target_State'), method = 'lm') +
+geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y = 'MME_Per_Capita',color = 'Is_Target_State'), method = 'lm') +
+geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y ='MME_Per_Capita',color = 'Is_Target_State'), method = 'lm') +
+#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y = 'MME_Per_Capita'), color = "black") +
+#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y ='MME_Per_Capita'),color = "black") +
+#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y = 'MME_Per_Capita'), color = "blue") +
+#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y ='MME_Per_Capita'),color = "blue") +
 
 geom_vline(xintercept = policy_year,colour="#BB0000") +
-ggtitle('FL Opioid Diff-in-Diff Analysis')
+ggtitle('FL Drug Shipment Diff-in-Diff Analysis')
 #ylim(0,1)
 )
-(ggsave(filename = 'FL_DiD_Analysis',plot = p,width =12, height = 8))
+(ggsave(filename = '../30_results/FL_Drug_DiD_Analysis',plot = p,width =12, height = 8))
 
 
 ## add propotional change later
