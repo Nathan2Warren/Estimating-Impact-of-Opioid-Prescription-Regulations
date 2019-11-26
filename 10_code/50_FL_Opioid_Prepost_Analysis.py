05aed30391564fac97e27626ee8c07d5e3df5889
jz263 <54601170+jz263@users.noreply.github.com>
2019-11-25 20:35:11 -0500

    updated all results and codes needed for report

--- 10_code/50_FL_Opioid_Prepost_Analysis.py
+++ 10_code/50_FL_Opioid_Prepost_Analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import glob
 import os
 from plotnine import *
+from sklearn import linear_model
 #define states, the first is study state, the latters are comparison states
 States = ['FL']
 policy_year = 2010
@@ -11,6 +12,8 @@ os.getcwd()
 
 
 Opioid_Mort_Census = pd.read_csv('45_Opioid_Mort_Census_cleaned.csv')
+Opioid_Mort_Census.loc[Opioid_Mort_Census['State']=='FL'].sort_values('MME',ascending=False)
+
 
 # subset needed states
 mask = Opioid_Mort_Census['State'].isin(States)
@@ -20,25 +23,29 @@ Opioid_Mort_Census['State'].value_counts()
 Opioid_Mort_Census = Opioid_Mort_Census.drop(columns = 'Unnamed: 0')
 
 # Calculate Deaths per 100,000 people & MME per capita
-Opioid_Mort_Census['MME_Per_Capita'] = Opioid_Mort_Census['MME']/Opioid_Mort_Census['Population']
+Opioid_Mort_Census['MME_Per_Capita'] = Opioid_Mort_Census['MME']/Opioid_Mort_Census['Population']*1000
 Opioid_Mort_Census['Deaths_Per_100000'] = Opioid_Mort_Census['Deaths']/Opioid_Mort_Census['Population']*100000
-Opioid_Mort_Census.to_csv('FL_Did_Opioid_Mort_Census.csv')
+#Opioid_Mort_Census.to_csv('FL_Did_Opioid_Mort_Census.csv')
 Opioid_Mort_Census['Is_Target_State'] = Opioid_Mort_Census['State'] == States[0]
+Opioid_Mort_Census['Extrapolate Prediction'] = Opioid_Mort_Census['Year'] >=policy_year
 # subtract comparison States and average over population
+lr = linear_model.LinearRegression()
+
+X = Opioid_Mort_Census.loc[Opioid_Mort_Census['Extrapolate Prediction'] !=True]['Year'].values.reshape(-1,1)
+y = Opioid_Mort_Census.loc[Opioid_Mort_Census['Extrapolate Prediction'] !=True]['MME_Per_Capita'].values.reshape(-1,1)
+
+lr.fit(X,y)
+Opioid_Mort_Census['Pred_MME_Per_Capita'] = lr.predict(Opioid_Mort_Census['Year'].values.reshape(-1,1)).flatten().T
 
 p = (ggplot(Opioid_Mort_Census,aes( x = 'Year', y ='MME_Per_Capita')) +
-#geom_line(Opioid_Mort_Census.loc[Opioid_Mort_Census['after_policy_year']==True], aes( x = 'Year', y ='Pred_Deaths_per_Pop'), color = "red") +
-#geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y = 'MME_Per_Capita', color = 'Is_Target_State'), method = 'lm') +
-#geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y ='MME_Per_Capita',color = 'Is_Target_State'), method = 'lm') +
-geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y = 'MME_Per_Capita',color = 'Is_Target_State'), method = 'lm') +
-geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y ='MME_Per_Capita',color = 'Is_Target_State'), method = 'lm') +
-#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y = 'MME_Per_Capita'), color = "black") +
-#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==False)], aes( x = 'Year', y ='MME_Per_Capita'),color = "black") +
-#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y = 'MME_Per_Capita'), color = "blue") +
-#geom_point(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1)) & (Opioid_Mort_Census['Is_Target_State']==True)], aes( x = 'Year', y ='MME_Per_Capita'),color = "blue") +
-
+geom_line(Opioid_Mort_Census.loc[Opioid_Mort_Census['Year']>=policy_year], aes( x = 'Year', y ='Pred_MME_Per_Capita',colour = 'Extrapolate Prediction'), linetype = "dashed")+
+geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']>=policy_year)], aes( x = 'Year', y = 'MME_Per_Capita'),color = "#5bd4db",size =1, method = 'lm') +
+geom_smooth(Opioid_Mort_Census.loc[(Opioid_Mort_Census['Year']<=(policy_year-1))], aes( x = 'Year', y ='MME_Per_Capita',colour = 'Extrapolate Prediction'), method = 'lm') +
+theme(legend_position=(0.5,-0.01)) +
+#labs(fill = "Counties in State with Policy Change") +
+ylab("MME Per Capita")  +
 geom_vline(xintercept = policy_year,colour="#BB0000") +
-ggtitle('FL Drug Shipment Pre-Post Analysis')
+ggtitle('Florida MME Dosage Pre-Post Analysis')
 #ylim(0,1)
 )
 (ggsave(filename = '../30_results/FL_Drug_Prepost_Analysis',plot = p,width =12, height = 8))
--- 10_code/51_State_Prepost_Analysis.py
+++ 10_code/51_State_Prepost_Analysis.py
@@ -2,14 +2,25 @@ import pandas as pd
 import numpy as np
 import os
 from plotnine import *
-
+import plotnine as plnine
+from sklearn import linear_model
 os.chdir('C:/Users/Jiajie Zhang/estimating-impact-of-opioid-prescription-regulations-team-3/20_intermediate_files')
 os.getcwd()
 #read in files
-df =  pd.read_csv('17_mortality_pop_county_level.csv')
-len(df.loc[df['State']=='FL']['County'].unique())
 
-def prepost_plot(Target_State, policy_year):
+df =  pd.read_csv('17_mortality_pop_county_level.csv')
+States = ['FL']
+
+# subset dataset
+mask = df['State'].isin(States)
+Mortality_Pop = df.loc[mask]
+policy_year = 2010
+full_name = 'FL'
+# calculate deaths per poppulation
+Mortality_Pop['Deaths_Per_100000'] = Mortality_Pop['Deaths']/Mortality_Pop['Population']*100000
+Mortality_Pop['Is_Target_State'] = Mortality_Pop['State'] == States[0]
+
+def prepost_plot(Target_State, full_name, policy_year):
     ###read dataset
     df =  pd.read_csv('17_mortality_pop_county_level.csv')
     States = [Target_State]
@@ -21,18 +32,30 @@ def prepost_plot(Target_State, policy_year):
     # calculate deaths per poppulation
     Mortality_Pop['Deaths_Per_100000'] = Mortality_Pop['Deaths']/Mortality_Pop['Population']*100000
     Mortality_Pop['Is_Target_State'] = Mortality_Pop['State'] == States[0]
+    Mortality_Pop['Extrapolate Prediction'] = Mortality_Pop['Year'] >=policy_year
+    #add prediction column on years after policy change
+    lr = linear_model.LinearRegression()
 
-    p = (ggplot() +
+    X = Mortality_Pop.loc[Mortality_Pop['Extrapolate Prediction'] !=True]['Year'].values.reshape(-1,1)
+    y = Mortality_Pop.loc[Mortality_Pop['Extrapolate Prediction'] !=True]['Deaths_Per_100000'].values.reshape(-1,1)
+
+    lr.fit(X,y)
+    Mortality_Pop['Pred_Deaths_per_100000'] = lr.predict(Mortality_Pop['Year'].values.reshape(-1,1)).flatten().T
+
+    p = (ggplot() +theme(legend_position=(0.5,-0.01)) +
         #geom_line(Mortality_Pop.loc[Mortality_Pop['after_policy_year']==True], aes( x = 'Year', y ='Pred_Deaths_per_Pop'), color = "red") +
-        geom_smooth(Mortality_Pop.loc[(Mortality_Pop['Year']>=policy_year) ], aes( x = 'Year', y = 'Deaths_Per_100000',color = 'Is_Target_State'), method = 'lm') +
-        geom_smooth(Mortality_Pop.loc[(Mortality_Pop['Year']<=(policy_year-1))], aes( x = 'Year', y ='Deaths_Per_100000',color = 'Is_Target_State'), method = 'lm') +
+        geom_line(Mortality_Pop.loc[Mortality_Pop['Year']>=policy_year], aes( x = 'Year', y ='Pred_Deaths_per_100000',colour = 'Extrapolate Prediction'), linetype = "dashed")+
+        #geom_line(Mortality_Pop.loc[(Mortality_Pop['Year']<=(policy_year-1))], aes( x = 'Year', y ='Deaths_Per_100000',colour = 'Before_PC')) +
+        geom_smooth(Mortality_Pop.loc[(Mortality_Pop['Year']>=policy_year) ], aes( x = 'Year', y = 'Deaths_Per_100000'),colour = '#5bd4db',size = 1,method = 'lm') +
+        geom_smooth(Mortality_Pop.loc[(Mortality_Pop['Year']<=(policy_year-1))], aes( x = 'Year', y ='Pred_Deaths_per_100000',colour = 'Extrapolate Prediction'), method = 'lm') +
+        #geom_point(Mortality_Pop.loc[(Mortality_Pop['Year']<=(policy_year-1))], aes( x = 'Year', y ='Deaths_Per_100000'),color = 'red') +
         geom_vline(xintercept = policy_year,colour="#BB0000") +
-        ggtitle('{} Overdose Mortality Prepost Analysis'.format(States[0]))
-        #ylim(0,1)
+        ylab("Deaths Per 100k")  +
+        ggtitle('{} Drug Overdose Mortality Prepost Analysis'.format(full_name)) +
+        scale_x_continuous(breaks = [2003,2006,2009,2012,2015])
     )
-
     (ggsave(filename = '../30_results/{}_Prepost_Analysis'.format(States[0]),plot = p,width =12, height = 8))
-
-prepost_plot('FL',2010)
-prepost_plot('TX',2007)
-prepost_plot('WA',2012)
+    return p
+p1 = prepost_plot('FL','Florida',2010)
+p2 = prepost_plot('TX','Texas',2007)
+p3 = prepost_plot('WA','Washington',2012)
