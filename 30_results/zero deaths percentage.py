0ebffb8bc2c19d3d7bece13fa6f3226a35487e97
Nathan2Warren <Nathan.warren222@gmail.com>
2019-11-26 20:03:53 -0500

    updated code by adding for loops with a list to give us back the proper metrics for our summary stats

--- 30_results/zero deaths percentage.py
+++ 30_results/zero deaths percentage.py
@@ -1,21 +1,22 @@
-
-
-### county with zero death
+# Counties with zero deaths
 import pandas as pd
+
+# Load and check years
 df = pd.read_csv('https://raw.githubusercontent.com/MIDS-at-Duke/estimating-impact-of-opioid-prescription-regulations-team-3/master/20_intermediate_files/17_mortality_pop_county_level.csv?token=AKAKVRFK2VSZEXYHCHZEHY254RN4S')
-df.head()
-print(df[(df.State == 'FL') & (df.Deaths == 0)].count() / df[df.State == 'FL']['Deaths'].count())
+df.head(40)
+df.tail(40)
 
-print(df[(df.State == 'TX') & (df.Deaths == 0)].count() / df[df.State == 'TX']['Deaths'].count())
+# List of States
+states = ['FL', 'TX', 'WA', 'PA', 'OH', 'IN', 'IL', 'GA']
 
-print(df[(df.State == 'WA') & (df.Deaths == 0)].count() / df[df.State == 'WA']['Deaths'].count())
+# Get Percentage of zero values
+for i in states:
+    print(df[(df.State == i) & (df.Deaths == 0)].count()) / df[df.State == i]['Deaths'].count())
 
-print(df[(df.State == 'PA') & (df.Deaths == 0)].count() / df[df.State == 'PA']['Deaths'].count())
+# Get count of all counties to ensure our data has the full number of counties
+for i in states:
+    print(df[df['State'] == i].count())
 
-print(df[(df.State == 'OH') & (df.Deaths == 0)].count() / df[df.State == 'OH']['Deaths'].count())
-
-print(df[(df.State == 'IN') & (df.Deaths == 0)].count() / df[df.State == 'IN']['Deaths'].count())
-
-print(df[(df.State == 'IL') & (df.Deaths == 0)].count() / df[df.State == 'IL']['Deaths'].count())
-
-print(df[(df.State == 'GA') & (df.Deaths == 0)].count() / df[df.State == 'GA']['Deaths'].count())
+# Get count of zero death counties
+for i in states:
+    print(df[(df.State == i) & (df.Deaths == 0)].count())
